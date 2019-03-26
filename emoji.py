2ea3b01439ce4b75325f2929014dbc71b5f583ad
baneenhamid <48797874+baneenhamid@users.noreply.github.com>
2019-03-26 21:46:55 +0300

    Add files via upload

---
+++ emoji.py
@@ -0,0 +1,20 @@
+print("grinning face","\U0001F600")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+print("smiling face with smiling eyes","\U0001F60A")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+print("smiling face with heart-eyes","\U0001F60D")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+print("squinting face with tongue","\U0001F61D")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+print("face with rolling eyes","\U0001F644")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+print("robot","\U0001F916")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+print("smiling cat with heart-eyes","\U0001F63B")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+print("woman technologist","\U0001F469")
+print("\U00002764","\U00002764","\U00002764","\U00002764")
+
+
+
+
